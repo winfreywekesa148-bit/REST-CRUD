93f2006f505000663696424a721ec6f08f8f027b
Winkesa <winfreywekesa148@gmail.com>
2026-06-28 21:27:46 +0300

    update

--- app.py
+++ app.py
@@ -26,6 +26,15 @@ def create_event():
     return jsonify(new_event.to_dict()), 201
 
 # TODO: PATCH /events/<id> - Update the title of an event
+@app.route("/events/<int:id>", methods=["PATCH"])
+def update_event(id):
+    data = request.get_json()
+    event = next((e for e in events if e.id == id), None)
+    if not event:
+        return ("Event not found", 404)
+    if "title" in data:
+        event.title = data["title"]
+    return jsonify(event.to_dict())
 
 # TODO: DELETE /events/<id> - Remove an event from the list
 
